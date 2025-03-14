7aabf5e359699ad4e8c0ff9a5bd60080c838a0bf
Juana Diaz <estidy@gmail.com>
2025-03-14 20:38:24 -0300

    creacion del proyecto e instalacion de tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+     <header className="bg-teal-400 py-5">
+      <h1 className="text-center text-4xl font-black"> Calculadora de Propinas y Consumos</h1>
+     </header>
+    </>
+  )
+}
+
+export default App
