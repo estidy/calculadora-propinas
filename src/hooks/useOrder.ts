775e0715ea42c7c54d2ac6e9e7972c6ab77a0107
Juana Diaz <estidy@gmail.com>
2025-03-15 17:16:16 -0300

    metodo agregarItem

---
+++ src/hooks/useOrder.ts
@@ -0,0 +1,25 @@
+import { useState } from "react"
+import type{MenuItem, OrderItem} from '../types'
+
+export default function useOrder() {
+    const [order, setOrder] = useState<OrderItem[]>([])
+
+    const addItem =(item : MenuItem)=>{
+        const itemExist = order.find(itemOrder=>itemOrder.id === item.id)
+        if (itemExist){
+            const updateOrder = order.map(itemOrder=>itemOrder.id === item.id ? 
+                {...itemOrder, quantity:itemOrder.quantity + 1} :itemOrder)
+            setOrder(updateOrder)
+        }
+        else{
+            const newItem = {...item, quantity:1}
+            setOrder([...order, newItem])
+        }
+        console.log(order)
+    }
+
+    return {
+        order,
+        addItem
+     }
+}
