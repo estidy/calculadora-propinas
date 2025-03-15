c1a59841ad6678da2fa1cedceb4431a425dcd47f
Juana Diaz <estidy@gmail.com>
2025-03-15 19:45:41 -0300

    componente para mostrar los totales que manejara el proyecto

---
+++ src/components/OrderTotals.tsx
@@ -0,0 +1,32 @@
+import { formatCurrency } from "../helpers"
+import type { OrderItem } from "../types"
+import { useMemo } from "react"
+
+type OrderTotalsProps = {
+  order:OrderItem[]
+}
+
+export default function OrderTotals({order}:OrderTotalsProps) {
+const subtotalAmount = useMemo(()=>order.reduce((total, item)=>total + (item.price * item.quantity),0), [order])
+
+  return (
+    <>
+      <div className="space -y-3">
+          <h2 className="font-black text-2xl">Totales y Propinas</h2>
+          <p> Subtotal a pagar: {' '}
+            <span className=" font-bold">{formatCurrency(subtotalAmount)}</span>
+          </p>
+          <p> Propina: {' '}
+            <span className=" font-bold">{formatCurrency(0)}</span>
+          </p>
+          <p> Total a pagar: {' '}
+            <span className=" font-bold">{formatCurrency(0)}</span>
+          </p>
+      </div>
+
+      <button>
+
+      </button>
+    </>
+  )
+}
