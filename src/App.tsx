95851307ed2c85e99da3f68bb985c97f333ed422
Juana Diaz <estidy@gmail.com>
2025-03-16 21:52:54 -0300

    agregando el componente de tipPercentageForm y demas totales

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import {menuItems} from './data/db'
 import useOrder from './hooks/useOrder'
 import OrderContents from './components/OrderContents'
 import OrderTotals from './components/OrderTotals'
+import TipPercentageForm from './components/TipPercentageForm'
 
 function App() {
 
-  const {order, addItem, removeItem} = useOrder()
+  const {order, tip, setTip, addItem, removeItem, placeOrder} = useOrder()
 
   return (
     <>
@@ -27,13 +28,25 @@ function App() {
             </div>
         </div>
         <div className='border border-dashed border-slate-200 p-5 rounded-lg space-y-10'>
-          <OrderContents
-             order = {order}
-             removeItem = {removeItem}
-          />
-          <OrderTotals 
-            order = {order}
-          />
+        {order.length > 0 ?( 
+            <>
+              <OrderContents
+                order = {order}
+                removeItem = {removeItem}
+              />
+              <TipPercentageForm
+              tip = {tip}
+              setTip={setTip}
+              />
+              <OrderTotals 
+                order = {order}
+                tip = {tip}
+                placeOrder = {placeOrder}
+              />
+            </>
+        ) : (
+            <p className="text-center">La lista esta vacia</p>            
+        )}
         </div>
         <div>
 
