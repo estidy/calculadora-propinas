b229d4862114bce0d4ff96c0d60007edc7a20321
Juana Diaz <estidy@gmail.com>
2025-03-16 21:55:45 -0300

    calculos para los totales y haciendo uso de la funcion en helpers para mostrarlo, de acuerdo a la modena seteada.

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -3,11 +3,15 @@ import type { OrderItem } from "../types"
 import { useMemo } from "react"
 
 type OrderTotalsProps = {
-  order:OrderItem[]
+  order: OrderItem[],
+  tip: number,
+  placeOrder: () => void
 }
 
-export default function OrderTotals({order}:OrderTotalsProps) {
-const subtotalAmount = useMemo(()=>order.reduce((total, item)=>total + (item.price * item.quantity),0), [order])
+export default function OrderTotals({order, tip, placeOrder}:OrderTotalsProps) {
+  const subtotalAmount = useMemo(()=>order.reduce((total, item)=>total + (item.price * item.quantity),0), [order])
+  const tipAmount = useMemo(()=>subtotalAmount * tip,[tip, order])
+  const totalAmount = useMemo(()=>subtotalAmount + tipAmount,[tip, order])
 
   return (
     <>
@@ -17,15 +21,19 @@ const subtotalAmount = useMemo(()=>order.reduce((total, item)=>total + (item.pri
             <span className=" font-bold">{formatCurrency(subtotalAmount)}</span>
           </p>
           <p> Propina: {' '}
-            <span className=" font-bold">{formatCurrency(0)}</span>
+            <span className=" font-bold">{formatCurrency(tipAmount)}</span>
           </p>
           <p> Total a pagar: {' '}
-            <span className=" font-bold">{formatCurrency(0)}</span>
+            <span className=" font-bold">{formatCurrency(totalAmount)}</span>
           </p>
       </div>
 
-      <button>
-
+      <button 
+          className = "w-full bg-black p-3 mt-10 uppercase text-white font-bold disabled:opacity-10"
+          disabled = {totalAmount === 0}
+          onClick={placeOrder}
+          >
+             Guardar Orden
       </button>
     </>
   )
